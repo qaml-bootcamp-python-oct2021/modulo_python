daf291b3469f1c8644ecd6fd99bf6ec0a21ee6c8
unknown <marielaGutiCall>
2021-10-19 22:07:39 -0400

    refactor solo operadores clase5-ejercicio3

--- clase_5/ejercicio-3.py
+++ clase_5/ejercicio-3.py
@@ -2,12 +2,19 @@ billetera = 200
 chocolate = 150
 agua = 100
 
-def imprimir_msg(total, saldo, dinero_suficiente):
+def imprimir_compra(total, saldo, dinero_suficiente):
     print(f"""Total de la compra: {total}
     Dinero Disponible en la billetera: {saldo} 
-    El usuario tiene suficiente dinero para la compra: {dinero_suficiente}""" ) 
-def calcular_compra_saldo(saldo, total):
-    imprimir_msg(total, saldo, saldo>=total)
+    El usuario tiene suficiente dinero para la compra?: {dinero_suficiente}""" ) 
+def calcular_compra(pro1, pro2):
+    return pro1 + pro2
+def calcular_si_hay_suficiente_dinero(saldo, total):
+    return saldo>=total
 
-calcular_compra_saldo(billetera,(chocolate+agua))
-calcular_compra_saldo(billetera,chocolate)
+total = calcular_compra(chocolate, agua)
+dinero_suficiente = calcular_si_hay_suficiente_dinero(billetera, total)
+imprimir_compra(total, billetera,dinero_suficiente)
+
+total = calcular_compra(chocolate, 0)
+dinero_suficiente = calcular_si_hay_suficiente_dinero(billetera, total)
+imprimir_compra(total, billetera,dinero_suficiente)
