f5149e0b733749a75b03b84d0f0d26b276ec94cf
Marcelo Equise <marcelo.xc.99@gmail.com>
2021-10-19 21:26:48 -0400

    add funciones de operadores y conversion a float

--- clase_05/ejercicios.py
+++ clase_05/ejercicios.py
@@ -8,3 +8,43 @@ print('''
     I Love
     Python
     <3 ''')
+
+a = 12.5
+b = 34
+
+#funcion suma
+def sumar(num_a, num_b):
+    resultado = num_a + num_b
+    return resultado
+
+#funcion resta
+def restar(num_a, num_b):
+    resultado = num_a - num_b
+    return resultado
+
+#funcion multiplicacion
+def multiplicar(num_a, num_b):
+    resultado = num_a * num_b
+    return resultado
+
+#funcion division positiva
+def dividir(num_a, num_b):
+    resultado = num_b / num_a
+    return resultado
+
+#funcion modulo b entre a
+def sacar_modulo(num_a, num_b):
+    resultado = num_b / num_a
+    return resultado
+
+#funcion convertir entero a flotante
+def to_float(num):
+    resultado = float(num)
+    return resultado
+
+print(sumar(a,b))
+print(restar(a,b))
+print(multiplicar(a,b))
+print(dividir(a,b))
+print(sacar_modulo(a,b))
+print(to_float(b))
