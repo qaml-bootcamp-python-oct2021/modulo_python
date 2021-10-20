00f5ffb2677db7a3f58828b35f98d737e18b51c9
Antonieta De La Torre <tonycol1984@gmail.com>
2021-10-19 20:28:51 -0500

    Add ejercicios dia 5

---
+++ clase_05/ejercicios.py
@@ -0,0 +1,9 @@
+#Ejercicio 5.01 - Tony De La Torre.
+
+print('Bienvenidos a \'QA\' "Minds"!!!')
+
+texto = """I Love
+Python
+'<3'"""
+
+print(texto)
---
+++ clase_05/ejercicios_2.py
@@ -0,0 +1,39 @@
+#Ejercicio 5.02 - Tony De La Torre
+
+a = 12.5
+b = 34
+
+def suma(num1,num2):
+    res = num1 + num2
+    return res
+
+def resta(num1,num2):
+    res = num1 - num2
+    return res
+
+def mult(num1,num2):
+    res = num1 * num2
+    return res
+
+def div(num1,num2):
+    res = num1 / num2
+    return res
+
+def modulo(num1,num2):
+    res = num1 % num2
+    return res
+
+resultado = suma(a,b)
+print(f"Suma de {a} y {b} es {resultado}")
+
+resultado = resta(a,b)
+print(f"Resta de {a} y {b} es {resultado}")
+
+resultado = mult(a,b)
+print(f"Multiplicación de {a} por {b} es {resultado}")
+
+resultado = div(a,b)
+print(f"División de {a} entre {b} es {resultado}")
+
+resultado = modulo(a,b)
+print(f"Módulo de {a} entre {b} es {resultado}")
