fff29f7ef0b72153b540d0f27afa42266512aed6
brosquero <broortiz93@gmail.com>
2021-10-19 20:47:16 -0500

    clase05-ejercicio 03

---
+++ clase_05/ejercicio_03.py
@@ -0,0 +1,12 @@
+def compra(saldo,chocolate, agua):
+    costo_total= chocolate+agua
+    compra_posible=  saldo>= costo_total
+    print(f'Saldo inicial = {saldo}')
+    print(f'Costo total = {costo_total}')
+    print(f'Compra es posible = {compra_posible}')
+    if(compra_posible):
+        saldo_actual= saldo- costo_total
+        print(f'Saldo despues de la compra = {saldo-costo_total}')
+
+compra(200, 150, 100)
+compra(200, 150, 0)
