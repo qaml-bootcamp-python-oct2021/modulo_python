4aa4dfb934714936d53f26b6cccdd92db85ac67b
Juan Luis Larios <larioszac@gmail.com>
2021-10-19 20:49:21 -0500

    add ejercicio3

--- clase_05/ejercicios.py
+++ clase_05/ejercicios.py
@@ -34,3 +34,23 @@ print(multiplicacion(a,b))
 print(division(a,b))
 print(modulo(a,b))
 print(convertir(7))
+
+
+cantidad_billetera = 200
+chocolate = 150
+agua = 100
+
+def compra1(cantidad_billetera,chocolate,agua):
+    total_compra = chocolate + agua
+    puedo_comprar = cantidad_billetera >= total_compra
+    print(f'¿Me alcanza el dinero para un chocolate y agua? {puedo_comprar}')
+    print(f'El total de la compra es: {total_compra}')
+
+def compra2(cantidad_billetera,chocolate,agua):
+    total_compra = chocolate
+    puedo_comprar = cantidad_billetera >= total_compra
+    print(f'¿Me alcanza el dinero para un chocolate? {puedo_comprar}')
+    print(f'El total de la compra es: {total_compra}')
+
+compra1(cantidad_billetera,chocolate,agua)
+compra2(cantidad_billetera,chocolate,agua)
