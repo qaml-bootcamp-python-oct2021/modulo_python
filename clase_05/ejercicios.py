15cc840e98cfd64436f84db0f6672a394f8bc105
Juan Luis Larios <larioszac@gmail.com>
2021-10-19 20:28:15 -0500

    add ejercicio2

--- clase_05/ejercicios.py
+++ clase_05/ejercicios.py
@@ -1,2 +1,36 @@
 print('''Bienvenidos a 'QA' "Minds"!!!''')
 print('''"I Love\nPython\n'<3' "''')
+
+a = 12.5
+b = 34
+
+def suma(a,b):
+    total = a + b
+    return total
+
+def resta(a,b):
+    total = a - b
+    return total
+
+def multiplicacion(a,b):
+    total = a * b
+    return total
+
+def division(a,b):
+    total = b//a
+    return total
+
+def modulo(a,b):
+    total = b % a
+    return total
+
+def convertir(num):
+    conversion = float(num)
+    return conversion
+
+print(suma(a,b))
+print(resta(a,b))
+print(multiplicacion(a,b))
+print(division(a,b))
+print(modulo(a,b))
+print(convertir(7))
