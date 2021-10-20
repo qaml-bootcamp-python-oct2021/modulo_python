693a81f1c54e033d9460c872440e5540bf0a9c53
Marcelo Equise <marcelo.xc.99@gmail.com>
2021-10-19 21:54:37 -0400

    add scripts

--- clase_05/ejercicios.py
+++ clase_05/ejercicios.py
@@ -48,3 +48,32 @@ print(multiplicar(a,b))
 print(dividir(a,b))
 print(sacar_modulo(a,b))
 print(to_float(b))
+
+#script compra de 2 productos
+billetera = 200
+chocolate = 150
+agua = 100
+
+def comprar_2_productos(producto_1, producto_2):
+    costo_total = producto_1 + producto_2
+    return costo_total
+
+def calcular_saldo(billetera, costo_total):
+    saldo = billetera - costo_total
+    return saldo
+
+def comprar_1_producto(producto_1):
+    costo_total = producto_1
+    return costo_total
+
+#print costo total
+print(f'costo total es {comprar_2_productos(chocolate, agua)}')
+#print dinero disponible
+print(f'dinero disponible es {billetera}')
+#print el usuario tiene dinero suficiente
+print(f'puede comprar? {(comprar_2_productos(chocolate,agua)<billetera)}')
+
+#segundo escenario
+print("segundo escenario")
+print(f'puede comprar? {(comprar_1_producto(chocolate)<billetera)}')
+print(f'puede comprar? {(comprar_1_producto(agua)<billetera)}')
