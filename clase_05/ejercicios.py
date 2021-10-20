2e2b577bfa10349da11bd8cfccd4733840f131f8
Marcelo Equise <marcelo.xc.99@gmail.com>
2021-10-19 21:13:56 -0400

    add ejercicios imprimir cadenas de texto

---
+++ clase_05/ejercicios.py
@@ -0,0 +1,10 @@
+#imprime la cadena: Bienvenidos a 'QA' "Minds"!!!
+print("""Bienvenidos a 'QA' "Minds"!!!""")
+
+#Imprime la cadena: "I Love
+#                   Python
+#                   <3"
+print('''
+    I Love
+    Python
+    <3 ''')
