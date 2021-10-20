c98729abeeedbac455ef45ad02752816fbe2531d
unknown <marielaGutiCall>
2021-10-19 22:40:49 -0400

    refactor saldo del ejercicio

--- clase_5/ejercicio-3.py
+++ clase_5/ejercicio-3.py
@@ -2,19 +2,26 @@ billetera = 200
 chocolate = 150
 agua = 100
 
-def imprimir_compra(total, saldo, dinero_suficiente):
-    print(f"""Total de la compra: {total}
-    Dinero Disponible en la billetera: {saldo} 
-    El usuario tiene suficiente dinero para la compra?: {dinero_suficiente}""" ) 
+
 def calcular_compra(pro1, pro2):
     return pro1 + pro2
 def calcular_si_hay_suficiente_dinero(saldo, total):
     return saldo>=total
-
+def calcular_saldo_disponible(billetera, total):
+    return billetera-total
+def imprimir_compra(total, saldo, dinero_suficiente):
+    print(f"Total de la compra: {total}")
+    print(f"Saldo Disponible: {saldo}")
+    print(f"El usuario tiene suficiente dinero para la compra?: {dinero_suficiente}") 
+    
 total = calcular_compra(chocolate, agua)
+saldo = calcular_saldo_disponible(billetera,total)
 dinero_suficiente = calcular_si_hay_suficiente_dinero(billetera, total)
-imprimir_compra(total, billetera,dinero_suficiente)
+imprimir_compra(total, saldo,dinero_suficiente)
 
-total = calcular_compra(chocolate, 0)
+print('-------------------------')
+
+total = agua
+saldo = calcular_saldo_disponible(billetera,total)
 dinero_suficiente = calcular_si_hay_suficiente_dinero(billetera, total)
-imprimir_compra(total, billetera,dinero_suficiente)
+imprimir_compra(total, saldo,dinero_suficiente)
