e60eabc44e127d103d06c8abd144abbde10a0dcc
brosquero <broortiz93@gmail.com>
2021-10-19 20:29:42 -0500

    clase05- ejercicio 02

---
+++ clase_05/ejercicio_02.py
@@ -0,0 +1,23 @@
+def suma(a,b):
+    return a+b
+
+def resta(a,b):
+    return b-a
+
+def multiplicacion(a,b):
+    return a*b
+
+def div(a,b):
+    return b/a
+
+def modulo(a,b):
+    return b%a
+
+def int_to_float(num_int):
+    return float(num_int)
+
+print  (f'Suma:{suma(12.5,34)}')   
+print  (f'Resta:{resta(12.5,34)}')   
+print  (f'Multiplicación:{multiplicacion(12.5,34)}')   
+print  (f'Mod:{modulo(12.5,34)}')   
+print  (f'Int to float:{int_to_float(10)}') 
