a8c6602ae4aefb24e1e24eadc7adbdc9490bd3fc
Clara21tg <clara21tg@gmail.com>
2021-10-21 13:42:50 -0500

    ctg

--- clase_05/ejercicios.py
+++ clase_05/ejercicios.py
@@ -1,4 +1,8 @@
+#Imprime la cadena: Bienvenidos a 'QA' "Minds"!!!
 print ("Bienvenidos a \'QA\' \"Minds\"!!!" )
+print("-----------")
+#Imprime la cadena: "I Love Python '<3'"
 print ("""\"I Love 
 Python 
 \'<3\'\"""")
+
