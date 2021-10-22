e3438a927a46be2fb9cb6112d0e06c146999408a
Cristian <cristianbj@outlook.com>
2021-10-21 20:09:57 -0500

    condicionales meses

---
+++ clase_05/practica01.py
@@ -0,0 +1,14 @@
+mes = 0
+
+if mes >= 3 and mes <= 5:
+    print('Estamos en Primavera')
+elif mes >= 6 and mes <= 8:
+    print('Estamos en Verano')
+elif mes >= 9 and mes <= 11:
+    print('Estamos en otoño')
+elif mes >= 12 and mes <= 2:
+    print('Estamos en Invierno')
+else:
+    print('El mes ingresado no existe')
+    print('Por favor verifique e intente de nuevo')
+    print('\'La Gerencia\'')
