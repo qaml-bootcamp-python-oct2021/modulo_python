fa7ae822e9078c459aee0c0fba20e6d834664dae
Marcelo Equise <marcelo.xc.99@gmail.com>
2021-10-21 22:24:06 -0400

    refactor methods

--- clase_06/ejercicios_03.py
+++ clase_06/ejercicios_03.py
@@ -6,16 +6,20 @@ current_year = datetime.date.today().year
 
 dia = 3
 mes = 8
-anio = 1999
+anio = 1987
+
+def ya_cumplio_anios(mes, dia):
+    return mes < current_month and dia < current_day
 
 def calcular_edad(dia, mes, anio):
     edad = current_year - anio -1
-    if mes < current_month and dia < current_day:
+    if ya_cumplio_anios(mes, dia):
         edad = edad + 1
-        print('ya cumplio años este año')
+        message = 'ya cumplio años este año'
     else:
-        print('no cumplio años este año todavia')
+        message = 'no cumplio años este año todavia'
     print(edad)
+    print(message)
 
 def saber_generacion(anio):
     if anio >=1920 and anio <= 1939:
