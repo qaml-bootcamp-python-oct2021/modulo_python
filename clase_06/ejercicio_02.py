8880811d98633cb8bf9967b84ba65d04d9a9e762
brosquero <broortiz93@gmail.com>
2021-10-21 20:37:39 -0500

    Clase 06: ejercio 2

---
+++ clase_06/ejercicio_02.py
@@ -0,0 +1,18 @@
+def validar_edad(edad):
+     return (edad>14)
+
+def validar_estatura(estatura): 
+     return (estatura>=1.62)
+
+
+def subir_montania(edad, altura):
+    if validar_edad(edad) and validar_estatura(altura):
+         print("Puede subir a la montaña rusa")
+    else: 
+        print("No puede subir a la montaña rusa debido a:")
+        if not validar_edad(edad): 
+            print("No cumple con la edad minima")
+        if not validar_estatura(altura):
+            print ("No cumple con la altura minima")
+
+subir_montania(15,1.62)
