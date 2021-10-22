13b5d6533c5e3cab5e21479e1318ca824062e1aa
Cristian <cristianbj@outlook.com>
2021-10-21 21:43:01 -0500

    update

--- clase_06/practica03.py
+++ clase_06/practica03.py
@@ -6,16 +6,19 @@ current_year = datetime.date.today().year
 
 dia = 20
 mes = 8
-anio = 1900
+anio = 2025
 
 def edadPersona(anio,current_year):
-    if anio >= 1920:
+    if anio >= 1920 and anio <= current_year:
         edad = current_year - anio
         print(f'La persona tiene {edad} año(s)')
-        cumplioAños(mes,current_month)
-        generacion(anio,current_year)
+        if anio == current_year:
+            generacion(anio,current_year)
+        else:
+            cumplioAños(mes,current_month)
+            generacion(anio,current_year)
     else:
-        print(f'Ingrese una fecha de nacimiento mayor a 1920')
+        print(f'Ingrese una fecha de nacimiento mayor a 1920 y menor o igual a {current_year}')
 
 
 def cumplioAños(mes,current_month):
@@ -26,15 +29,15 @@ def cumplioAños(mes,current_month):
 
 def generacion(anio,current_year):
     if anio >= 1920 and anio <= 1939:
-        print('Generacion silenciosa')
+        print('Pertenece a la \"Generacion silenciosa\"')
     elif anio >= 1940 and anio <= 1959:
-        print('Baby boomer')
+        print('Pertenece a la generacion de los \"Baby boomer\"')
     elif anio >= 1960 and anio <= 1979:
-        print('Generacion X')
+        print('Pertenece a la \"Generacion X\"')
     elif anio >= 1980 and anio <= 1989:
-        print('Generacion Y (Millenial)')
-    elif anio >= 1990 and anio < current_year:
-        print('Generacion Z')
+        print('Pertenece a la \"Generacion Y (Millenial)\"')
+    elif anio >= 1990 and anio <= current_year:
+        print('Pertenece a la \"Generacion Z')
 
 
 edadPersona(anio,current_year)
