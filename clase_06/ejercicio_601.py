143bb821cf6f5f8dc0bbdca69cf1e6ad42e82467
Antonieta De La Torre <tonycol1984@gmail.com>
2021-10-21 20:11:48 -0500

    update ejercicio 6.1

--- clase_06/ejercicio_601.py
+++ clase_06/ejercicio_601.py
@@ -3,16 +3,16 @@
 month = 2
 
 def season(mes):
-    if mes >=3 and mes <=5:
+    if (mes <1) or (mes>12):
+        print("""El mes ingresado no existe.
+Por favor, verifique e intente de nuevo.
+'La Gerencia'""")
+    elif mes >=3 and mes <=5:
         print("Estamos en Primavera :)")
     elif mes >=6 and mes <=8:
         print("Estamos en Verano :)")
     elif mes >=9 and mes <=11:
         print("Estamos en Otoño :)")
-    elif (mes <1) or (mes>12):
-        print("""El mes ingresado no existe.
-Por favor, verifique e intente de nuevo.
-'La Gerencia'""")
     else:
         print("Estamos en Invierno :)")
 
