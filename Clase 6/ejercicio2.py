483c94bd8169255bf4d000fb1a1151268a6ada9c
Ivan de la fuente <ivan.delafuente@6connex.com>
2021-10-21 20:47:49 -0500

    Clase6

--- Clase 6/ejercicio2.py
+++ Clase 6/ejercicio2.py
@@ -1,5 +1,5 @@
-edad= 10
-altura= 1.30
+edad= 14
+altura= 1.70
 
 if edad <14 and altura< 1.62:
     print("Altura y edad no validos")
@@ -7,5 +7,7 @@ elif edad <14:
     print("Edad no valida")
 elif altura < 1.62:
     print("Altura no valida")
+else:
+    print("diviertete")
 
 
