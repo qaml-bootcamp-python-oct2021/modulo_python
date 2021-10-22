9b5788b48d09325dbe334f26befec8ba39f329fa
Antonieta De La Torre <tonycol1984@gmail.com>
2021-10-21 19:56:02 -0500

    add ejercicio 6.1

---
+++ clase_06/ejercicio_601.py
@@ -0,0 +1,19 @@
+#Ejercicio 6.01 - Tony De La Torre
+
+month = -3
+
+def season(mes):
+    if mes >=3 and mes <=5:
+        print("Estamos en Primavera :)")
+    elif mes >=6 and mes <=8:
+        print("Estamos en Verano :)")
+    elif mes >=9 and mes <=11:
+        print("Estamos en Otoño :)")
+    elif (mes ==12) or (mes==1) or (mes ==2):
+        print("Estamos en Invierno :)")
+    else:
+        print("""El mes ingresado no existe.
+Por favor, verifique e intente de nuevo.
+'La Gerencia'""")
+
+season(month)
