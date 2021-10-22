10cad6bc1ed8483986dbb3fb205b8ca0c2e58a11
Brenda Montiel Chavez <brenda.montiel@ibm.com>
2021-10-21 19:10:00 -0600

    update on invierno and otoño

--- clase_06/ejercicio01.py
+++ clase_06/ejercicio01.py
@@ -1,11 +1,11 @@
 def temporada(mes):
-    if mes == 12 or mes ==1 and mes==2:
+    if mes == 12 or mes ==1 or mes==2:
         print ("Es invierno")
     elif mes >=3 and mes <= 5:
         print("Es primavera")
     elif mes >= 6 and mes <= 8:
         print ("Es verano")
-    elif mes >= 9 and mes <= 12:
+    elif mes >= 9 and mes <= 11:
         print ("Es otoño")
     else:
         print('''El mes ingresado no existe
