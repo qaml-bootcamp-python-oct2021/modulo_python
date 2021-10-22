adf2e53ba699828ca74ba33ce487c0c35f5f5119
Juan Luis Larios <larioszac@gmail.com>
2021-10-21 19:59:20 -0500

    add ejercicio 1

---
+++ clase_06/ejercicio_01.py
@@ -0,0 +1,14 @@
+mes = 1
+
+if mes == 3 or mes == 4 or mes == 5:
+    print('Estamos en Primavera')
+elif mes == 6 or mes == 7 or mes == 8:
+    print('Estamos en Verano')
+elif mes == 9 or mes == 10 or mes == 11:
+    print('Estamos en Otoño')
+elif mes == 12 or mes == 1 or mes == 2:
+    print('Estamos en Invierno')
+else:
+     print('''Mes ingresado no existe.
+     Por favor verifique e intente de nuevo.
+     'La Gerencia' ''')
