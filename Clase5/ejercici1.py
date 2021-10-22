c0df73d4f8140387e089cb255c02e2a273b55ea0
Ivan de la fuente <ivan.delafuente@6connex.com>
2021-10-21 22:13:58 -0500

    Clase5

---
+++ Clase5/ejercici1.py
@@ -0,0 +1,6 @@
+print('''Bienvenidos a 'QA'
+"Minds"!!!''')
+
+print('''"I Love
+Python
+ '<3' "''')
---
+++ Clase5/ejercicio2.py
@@ -0,0 +1,23 @@
+a = 12.5
+b = 34
+
+def suma(a,b):
+    return a + b
+
+def resta (a,b):
+    return a - b
+
+def multiplicacion(a,b):
+    return a * b
+
+def division(a,b):
+    return b/a
+
+def modulo(a,b):
+    return b%a
+
+print(f'La suma es {suma(a,b)}')
+print(f'La resta es {resta(a,b)}')
+print(f'La multiplicación es {multiplicacion(a,b)}')
+print(f'La división es {division(a,b)}')
+print(f'El módulo es {modulo(a,b)}')
