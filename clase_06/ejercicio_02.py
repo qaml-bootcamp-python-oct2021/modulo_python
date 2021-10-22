1a557345190070efe534fb03518c56f591182f83
brosquero <broortiz93@gmail.com>
2021-10-21 21:29:01 -0500

    cambios ejercicio 2

--- clase_06/ejercicio_02.py
+++ clase_06/ejercicio_02.py
@@ -9,10 +9,10 @@ def subir_montania(edad, altura):
     if validar_edad(edad) and validar_estatura(altura):
          print("Puede subir a la montaña rusa")
     else: 
-        print("No puede subir a la montaña rusa debido a:")
+        print("No puede subir a la montaña rusa debido a ")
         if not validar_edad(edad): 
             print("No cumple con la edad minima")
         if not validar_estatura(altura):
             print ("No cumple con la altura minima")
 
-subir_montania(15,1.62)
+subir_montania(15,1.70)
