76c5333aed5d785bc7e660397fe90a7307be205d
Ivan de la fuente <ivan.delafuente@6connex.com>
2021-10-21 20:21:13 -0500

    clase6

--- Clase 6/ejercicio1.py
+++ Clase 6/ejercicio1.py
@@ -8,7 +8,7 @@ elif mes >=6 and mes<=8:
     print("Estamos en Verano")
 elif mes >=9 and mes<=11:
     print("Estamos en Otoño")
-elif mes == 12 or mes >=1 and mes<=2 :
+elif mes  ==12 or mes >=1 and mes<=2 :
     print("Estamos en Invierno")
 else: 
     print("""El mes ingresado no existe
