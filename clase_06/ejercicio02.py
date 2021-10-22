f79ec874d697ceba92d957835ad51ca10775d435
Brenda Montiel Chavez <brenda.montiel@ibm.com>
2021-10-21 19:59:18 -0600

    update after comments on class

--- clase_06/ejercicio02.py
+++ clase_06/ejercicio02.py
@@ -2,26 +2,20 @@ edad_persona = 14
 tamaño_persona = 1.55
 
 def validar_edad(edad):
-    if edad > 14:
-        return True
-    else:
-        return False
+    return edad > 14
 
 def validar_tamaño(tamaño):
-    if tamaño_persona>=1.62:
-        return True
-    else:
-        return False
-
+    return tamaño >= 1.62
+       
 def subir_montaña_rusa(edad_montaña,tamaño_montaña):
     edad_valida = validar_edad(edad_montaña)
     tamaño_valido = validar_tamaño(tamaño_montaña)
 
-    if edad_valida == True and tamaño_valido == True:
+    if edad_valida and tamaño_valido :
         print('Sí se puede subir a la montaña rusa "Push-Pull"')
-    elif edad_valida == True and tamaño_valido == False:
+    elif edad_valida and not tamaño_valido:
          print('No se puede subir a la montaña rusa "Push-Pull" porque no tiene el tamaño necesario')
-    elif edad_valida == False and tamaño_valido == True:
+    elif not edad_valida and tamaño_valido:
         print('No se puede subir a la montaña rusa "Push-Pull" porque no tiene la edad necesaria')
     else:
         print('No se puede subir a la montaña rusa "Push-Pull" porque no tiene el tamaño ni edad necesarios')
