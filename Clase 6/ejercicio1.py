fe72a89a7009f04801b22404714cb97c94186087
Ivan de la fuente <ivan.delafuente@6connex.com>
2021-10-21 19:56:56 -0500

    Create ejercicio1.py

---
+++ Clase 6/ejercicio1.py
@@ -0,0 +1,16 @@
+
+
+mes = 12
+
+if mes >=1 and mes<=3:
+    print("Estamos en Primavera")
+elif mes >=4 and mes<=6:
+    print("Estamos en Verano")
+elif mes >=7 and mes<=9:
+    print("Estamos en Otoño")
+elif mes >=10 and mes <=12:
+    print("Estamos en Invierno")
+else: 
+    print("""El mes ingresado no existe
+             Por favor verifique e intente de nuevo
+             'La Gerencia' """)
