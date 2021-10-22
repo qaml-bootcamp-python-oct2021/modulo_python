f47149f9a09c5c3ea87c0739e250790d5c8ddd49
Marcelo Equise <marcelo.xc.99@gmail.com>
2021-10-21 21:59:30 -0400

    refactor method

--- clase_06/ejercicios_02.py
+++ clase_06/ejercicios_02.py
@@ -1,4 +1,4 @@
-edad = 15
+edad = 14
 altura = 1.62
 
 def verificar_edad(edad):
@@ -8,12 +8,15 @@ def verificar_altura(altura):
     return altura >= 1.62
 
 def puede_subir_montania_rusa (edad,altura):
+    
     if edad and altura:
         print('Puede subirse al "Push-Pull"')
-    elif edad:
+    elif edad and not altura:
         print('No tiene altura necesaria')
+    elif altura and not edad:
+        print('No tiene edad necesaria')
     else:
-        print('No tiene edad minima')
+        print('No cumple ninguno')
 
 edad_valida = verificar_edad(edad)
 altura_valida = verificar_altura(altura)
