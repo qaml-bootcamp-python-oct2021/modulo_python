fd2ff6c76f0c81f34436758e905e628a77fe6eaa
brosquero <broortiz93@gmail.com>
2021-10-23 09:25:51 -0500

    Sumatoria

---
+++ clase_07/ejercicio_01.py
@@ -0,0 +1,23 @@
+
+
+def cal_suma(numero):
+    contador=1
+    suma=0
+    while contador<numero:
+        suma+=contador
+        contador+=1
+        print(suma)
+    return suma
+
+
+def cal_suma_pares(numero):
+    contador=1
+    suma=0
+    while contador<numero:
+        if(contador%2==0):
+            suma+=contador
+        contador+=1
+        print(suma)
+    return suma
+
+print(f'Sumatoria: {cal_suma_pares()}')
