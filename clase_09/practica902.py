09b7aa6c8f5fa7d165674a5f1b3bea929a1c15f5
cristianbj <cristianbj@outlook.com>
2021-11-04 18:47:14 -0600

    practica09

--- clase_09/practica902.py
+++ clase_09/practica902.py
@@ -1,3 +1,4 @@
+'''
 agenda = {
     'contactos' : []
 }
@@ -25,11 +26,11 @@ def borrarContacto(nombre):
 
 
 menu = int(input('''
-    1. Crear contacto
-    2. Ver agenda
-    3. Buscar contacto
-    4. Borrar contacto
-    0. Salir    
+#    1. Crear contacto
+#    2. Ver agenda
+#    3. Buscar contacto
+#    4. Borrar contacto
+#    0. Salir    
 '''))
 
 while menu != 0:
@@ -45,3 +46,93 @@ while menu != 0:
         print('Ingrese una opcion valida')
         print(menu)
     break
+'''
+
+
+agenda = {
+    'contactos' : []
+}
+
+def crear_contacto():
+    nombre = input('Ingrese nombre de contacto\n')
+    telefono = input('Ingrese numero de telefono de contacto\n')
+    email = input('Ingrese email de contacto\n')
+    contacto = {
+        'nombre' : nombre,
+        'telefono' : telefono,
+        'email' : email
+    }
+    return contacto
+
+def guardar_contacto(contacto):
+    agenda['contactos'].append(contacto)
+
+def mostrar_agenda():
+    for contacto in agenda['contactos']:
+        mostrar_contacto(contacto)
+
+def get_contacto_a_buscar():
+    return input('Ingrese nombre de contacto a buscar\n')
+
+def buscar_contacto(nombre):
+    index = 0
+    while index < len(agenda['contactos']):
+        contactos = agenda['contactos'] 
+        contacto = contactos[index]
+        if contacto['nombre'] == nombre:
+            return contacto
+        index += 1
+    return None
+
+def mostrar_contacto(contacto : dict):
+    for key, value in contacto.items():
+        print(f'{key} - {value}')
+
+def eliminar_contacto(nombre):
+    index = 0
+    result = False
+    while index < len(agenda['contactos']) and not result:
+        contactos = agenda['contactos'] 
+        contacto = contactos[index]
+        if contacto['nombre'] == nombre:
+            contactos.pop(index)
+            result = True
+            print('Contacto eliminado')
+        index += 1
+    if not result:
+        print('No existe un contacto con el nombre ingresado. Intente de nuevo')
+
+opcion = int(input('''
+1 - Para crear contacto
+2 - Para visualizar Agenda
+3 - Para buscar contacto
+4 - Para eliminar contacto
+0 - Para salir
+'''))
+
+while opcion != 0:
+    if opcion == 1:
+        contacto = crear_contacto()
+        guardar_contacto(contacto)
+    elif opcion == 2:
+        mostrar_agenda()
+    elif opcion == 3:
+        nombre = get_contacto_a_buscar()
+        contacto = buscar_contacto(nombre)
+        if contacto is not None:
+            mostrar_contacto(contacto)
+        else:
+            print('El contacto ingresado no existe. Intente de nuevo')
+    elif opcion == 4:
+        nombre = get_contacto_a_buscar()
+        eliminar_contacto(nombre)
+    else:
+        print('opcion errada, intente de nuevo')
+
+    opcion = int(input('''
+1 - Para crear contacto
+2 - Para visualizar Agenda
+3 - Para buscar contacto
+4 - Para eliminar contacto
+0 - Para salir
+'''))
